c98b33204f690deba1cb57169ca5779986136c58
aluno23785 <aluno23785@ipt.pt>
2026-03-11 10:06:27 +0000

    classes iniciais do Model

---
+++ API/Models/MyUser.cs
@@ -0,0 +1,14 @@
+﻿namespace API.Models
+{
+    public class MyUser
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Address { get; set; }
+        public string postal_code { get; set; }
+        public string Country { get; set; }
+       
+        public string taxnumber { get; set; }
+        public string CellPhone { get; set; }
+    }
+}
